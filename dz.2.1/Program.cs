ed1b3647604d42343ed249c8520f2264a98fd37f
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-06 19:07:31 +0300

    Добавили решение первой задачи

--- dz.2.1/Program.cs
+++ dz.2.1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("111Hello, World!");
+﻿/* 
+__Задача 10:__ Напишите программу, которая принимает 
+на вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+    {
+    Console.WriteLine("Введено не трехзначное число ");
+    }
+else
+    {
+    Console.WriteLine(InCenter(number));
+    }
+int ReadInt(string message)
+    {
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+    }
+
+int InCenter(int a) //Функция выводит число, которое находится посередине.
+    {
+    
+    int result = ((a / 10) % 10);
+    return result;
+    }
