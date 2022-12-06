2d76823f226139e5f48c9849c6a64955ce1dce92
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-06 19:10:24 +0300

    Добавили решение третьей задачи

--- dz.2.3/Program.cs
+++ dz.2.3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message) //Вывод результатов того, что ввел пользователь
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 6 || a == 7)
+        {
+            Console.Write("Ура! " + a + " - Это - Выходной");
+        }
+        else
+        {
+            Console.Write("Жаль... " + a + " - Это - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число, которое входит за интервал от 1 до 7, поэтому, понять какой это день - невозможно...");
+    }
+    return " день.";
+}
